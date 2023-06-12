6319f2ee4838994e356c5808229a0079e05b86cf
Thanh2603 <86702148+Thanh2603@users.noreply.github.com>
2023-06-12 18:15:33 +0700

    Scroll Btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
@@ -9,8 +12,6 @@ import NotFound from "./pages/notFound/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FreeTrial from "./pages/freeTrial/FreeTrial";
-import { useEffect, useState } from "react";
-
 const App = () => {
   const handleGoToTop = () => {
     window.scrollTo({
@@ -47,16 +48,8 @@ const App = () => {
 
       <Footer />
       {showGoToTop && (
-        <button
-          onClick={handleGoToTop}
-          className="btn-scroll"
-          style={{
-            position: "fixed",
-            right: "20px",
-            bottom: "20px",
-          }}
-        >
-          Go To Top
+        <button onClick={handleGoToTop} className="btn-scroll" style={{}}>
+          {<BiArrowBack />}
         </button>
       )}
     </BrowserRouter>
